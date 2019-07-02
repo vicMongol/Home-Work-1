c47eb5cbe53ba02130087c82a36916defc4d7d4c
Victor Semehin <victorsemehin@gmail.com>
2019-07-02 03:35:56 +0300

    part HW 2

--- homework.js
+++ homework.js
@@ -1,42 +1,95 @@
-let i;
-let someNumber = prompt("Enter any number", "");
+// let i;
+// let someNumber = prompt("Enter any number", "");
 
-function isInteger(someNumber) {
-    return (someNumber ^ 0) === +someNumber;
-}
-console.log(isInteger(someNumber));
-if (someNumber > 0) {
-    console.log (true);
-} else {
-console.log (false);
-}
-if (someNumber < 0) {
-    console.log (true);
-} else {
-console.log (false);
-}
-if (+someNumber === 0) {
-    console.log (true);
-} else {
-console.log (false);
-}
+// function isInteger(someNumber) {
+//     return (someNumber ^ 0) === +someNumber;
+// }
+// console.log(isInteger(someNumber));
+// if (someNumber > 0) {
+//     console.log (true);
+// } else {
+// console.log (false);
+// }
+// if (someNumber < 0) {
+//     console.log (true);
+// } else {
+// console.log (false);
+// }
+// if (+someNumber === 0) {
+//     console.log (true);
+// } else {
+// console.log (false);
+// }
 
-//  короткая запись 
-i = (someNumber > 0) ? true : false;
-console.log(i);
-i = (someNumber < 0) ? true : false;
-console.log(i);
-i = (+someNumber === 0) ? true : false;
-console.log(i);
+// //  короткая запись 
+// i = (someNumber > 0) ? true : false;
+// console.log(i);
+// i = (someNumber < 0) ? true : false;
+// console.log(i);
+// i = (+someNumber === 0) ? true : false;
+// console.log(i);
 
-// Я не знал, что в консоль выводить булю или число, по этому...
+// // Я не знал, что в консоль выводить булю или число, по этому...
 
-i = (someNumber > 0) ? someNumber : false;
-console.log(i);
-i = (someNumber < 0) ? someNumber : false;
-console.log(i);
-i = (+someNumber === 0) ? true : false;
-console.log(i);
+// i = (someNumber > 0) ? someNumber : false;
+// console.log(i);
+// i = (someNumber < 0) ? someNumber : false;
+// console.log(i);
+// i = (+someNumber === 0) ? true : false;
+// console.log(i);
+
+// var simplyMass = ["ключи", "значение", "литерал", "функции", "конкотинация"];
+// var reversMass = [];
+// var x = simplyMass.pop();
+// // reversMass.unshift(x);
+// for ( i = 0; i < 5; i++ ) {
+//     //  var x = simplyMass.pop();
+//      reversMass.unshift(x);
+//} 
+
+
+// studens.sortfunction(user1, user2) {
+//     if (user1.name > user2.name) {
+//         return 1;
+//     } else if (user1.name == user2.name) {
+//     if (us1.name > us2.name) {
+//         return -1;
+//       } else 
+
+//     }
+// }
+
+// var xxx = [")", "Lol", "emanresu", "olleH"];
+//     xxx.splice(0, 4, "Hello username )");
+//     console.log(xxx.join(" "));
+
+// var xxx = [")", "Lol", "emanresu", "olleH"];
+//     xxx.splice(-3, 3, "Hello username");
+//     xxx.reverse();
+//     console.log(xxx.join(" "));
+
+// var xxx = [")", "Lol", "emanresu", "olleH"];
+// xxx.splice(1, 1);
+// var qqq = xxx[2].split('').reverse().join("");
+// var aaa = xxx[1].split('').reverse().join("");
+// console.log(qqq + " " + aaa + xxx[0]);
+
+
+
+var tabelStudens = [
+    {name: "Tetiana", mark: 4.8, diploma: true},
+    {name: "Andrew", mark: 5.0, diploma: false},
+    {name: "Maria", mark: 3.6, diploma: true},
+    {name: "Margo", mark: 4.8, diploma: false},
+    {name: "Sergey", mark: 4.9, diploma: false},
+    {name: "Alex", mark: 4.9, diploma: false},
+    {name: "Anatoly", mark: 4.9, diploma: true},
+];
+
+
+
+
+
 
 
 
